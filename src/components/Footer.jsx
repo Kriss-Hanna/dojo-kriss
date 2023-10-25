f3389efdc0b7109125cb166d3be52615b486bbbc
Kriss-Hanna <kristofferson.hanna@gmail.com>
2023-10-25 18:45:27 +0200

    add footer and style

---
+++ src/components/Footer.jsx
@@ -0,0 +1,61 @@
+const Footer = () => {
+  return (
+    <footer>
+      <div className="social-links">
+        <h3>Suivez-nous sur les réseaux !</h3>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Youtube</li>
+          <li>Whatsapp</li>
+          <li>Mail</li>
+        </ul>
+      </div>
+
+      <div className="details-links">
+        <div className="card-1">
+          <h3>A Propos</h3>
+          <ul>
+            <li>Politique de confidentialité</li>
+            <li>Conditions d'utilisation</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+          </ul>
+        </div>
+        <div className="card-1">
+          <h3>Stages</h3>
+          <ul>
+            <li>Politique de confidentialité</li>
+            <li>Conditions d'utilisation</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+          </ul>
+        </div>
+        <div className="card-1">
+          <h3>Réseaux sociaux</h3>
+          <ul>
+            <li>Politique de confidentialité</li>
+            <li>Conditions d'utilisation</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+          </ul>
+        </div>
+        <div className="card-1">
+          <h3>Liens Utiles</h3>
+          <ul>
+            <li>Politique de confidentialité</li>
+            <li>Conditions d'utilisation</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+            <li>Politique de confidentialité</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
